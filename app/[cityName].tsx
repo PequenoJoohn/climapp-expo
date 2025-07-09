91e6af630d75033f8e3811f6036d29cc1c619176
Jonathan Matos <jonathan.matos2445@gmail.com>
2025-07-09 15:54:44 -0300

    feat: add style to temperature page

--- app/[cityName].tsx
+++ app/[cityName].tsx
@@ -1,7 +1,8 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { LinearGradient } from "expo-linear-gradient";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const CityDetails = () => {
   const searchParams = useLocalSearchParams();
@@ -15,7 +16,7 @@ const CityDetails = () => {
       const responseJSON = await response.json();
 
       const city = responseJSON.find(
-        (cityData) => cityData.city === searchParams.cityName
+        (cityData: any) => cityData.city === searchParams.cityName
       );
 
       console.log(city);
@@ -33,7 +34,48 @@ const CityDetails = () => {
   return (
     <LinearGradient colors={["#00457d", "#05051f"]} style={styles.container}>
       <View>
-        <Text className={styles.headerTitle}>{cityDetails?.city}</Text>
+        <MaterialIcons
+          name="chevron-left"
+          size={24}
+          color={"#fff"}
+          style={styles.headerIcon}
+        />
+        <Text style={styles.headerTitle}>{cityDetails?.city}</Text>
+      </View>
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.cardHeaderTitle}>Hoje</Text>
+          <Text style={styles.cardHeaderTitle}>{cityDetails?.date}</Text>
+        </View>
+
+        <View style={styles.cardBox}>
+          <Image
+            source={require("../assets/images/clouds.png")}
+            style={styles.cardImage}
+          />
+
+          <View>
+            <Text style={styles.cardTemperature}>{cityDetails?.temp}º</Text>
+            <Text style={styles.cardDescription}>
+              {cityDetails?.description}
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.rowBox}>
+          <View style={styles.row}>
+            <Image source={require("../assets/icons/humidity.png")} />
+            <Text style={styles.rowTitle}>Humidity:</Text>
+            <Text style={styles.rowValue}>{cityDetails?.humidity}%</Text>
+          </View>
+          <View style={styles.row}>
+            <Image source={require("../assets/icons/temperature.png")} />
+            <Text style={styles.rowTitle}>Min/Max:</Text>
+            <Text style={styles.rowValue}>
+              {cityDetails?.forecast[0].min}/{cityDetails?.forecast[0].max}
+            </Text>
+          </View>
+        </View>
       </View>
     </LinearGradient>
   );
@@ -44,6 +86,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 16,
     paddingTop: 40,
+    gap: 40,
   },
   headerContainer: {
     alignItems: "center",
@@ -53,9 +96,70 @@ const styles = StyleSheet.create({
   headerTitle: {
     color: "#fff",
     fontSize: 20,
-    fontFamily: "Montserrat_500Medium",
+    fontFamily: "Montserrat_600SemiBold",
     textAlign: "center",
   },
+  card: {
+    width: "100%",
+    borderRadius: 24,
+    backgroundColor: "#4463D5",
+    padding: 16,
+    gap: 24,
+  },
+  cardHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+  cardHeaderTitle: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "Montserrat_600SemiBold",
+  },
+  headerIcon: {
+    position: "absolute",
+    left: 0,
+  },
+  cardImage: {
+    width: 72,
+    height: 64,
+  },
+  cardTemperature: {
+    color: "#fff",
+    fontSize: 43,
+    fontFamily: "Montserrat_700Bold",
+    textAlign: "center",
+  },
+  cardDescription: {
+    color: "#fff",
+    fontSize: 13,
+    fontFamily: "Montserrat_400Regular",
+    textAlign: "center",
+  },
+  cardBox: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8
+  },
+  rowTitle: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "Montserrat_600SemiBold"
+  }, 
+  rowValue: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "Montserrat_400Regular",
+    marginLeft: "auto"
+  },
+  rowBox: {
+    gap: 8
+  }
 });
 
 export default CityDetails;
